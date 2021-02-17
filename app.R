7f7da9e072f8ed777622760261dcfbeabf6256ee
sciav <42166022+sciav@users.noreply.github.com>
2021-02-16 23:23:48 -0800

    Add files via upload
    Init

---
+++ app.R
@@ -0,0 +1,122 @@
+library(shiny)
+library(shinyjs)
+library(DT)
+load("reapp.RData")
+
+source("apportion.R")
+source("chooser.R")
+states <- states[complete.cases(states), ]
+min_seats <- 50
+max_seats <- 1e5
+
+ui <- fluidPage(
+    useShinyjs(),
+    titlePanel("Reapportionment Simulator"),
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("seats",
+                         sprintf("Number of House seats (%d-%d)",
+                                 min_seats,
+                                 max_seats),
+                         435,
+                         min = min_seats,
+                         max = max_seats),
+            selectInput("year",
+                        "Population Year",
+                        choices = sort(unique(states[["Year"]])),
+                        selected = "2010"),
+            uiOutput("add_state"),
+            "Population data from:",
+            HTML("<a href = https://www.census.gov/data/tables/time-series/demo/popest/2010s-state-total.html>www.census.gov</a>")
+        ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Apportionment",
+                         "Apportionment formula:",
+                         HTML("<a href = https://www.everycrsreport.com/files/20130802_R41357_06aeca1ea6c97545c522ec0aedf047b705d37f69.html>www.everycrsreport.com</a>"),
+                         DT::dataTableOutput("apportionment")),
+                tabPanel("Historical Election Results",
+                         selectInput("election",
+                                     "Election Year",
+                                     choices = unique(pres_results[["Year"]]),),
+                         uiOutput("award_pr"),
+                         verbatimTextOutput("election_results"),
+                         DT::dataTableOutput("er"),
+                         "Election data from:",
+                         HTML("<a href = https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/42MVDX>dataverse.harvard.edu</a>")),
+                tabPanel("Election Simulator",
+                         uiOutput("chooser"),
+                         verbatimTextOutput("custom_election")
+                         )
+
+
+            ))
+    )
+)
+server <- function(input, output, session) {
+    state_data <- reactive({
+        year <- input[["year"]]
+        droplevels(states[states[["Year"]] == year, ])
+    })
+    election_results <- reactive({
+        year <- input[["election"]]
+        pres_results[pres_results[["Year"]] == year, ]
+    })
+    app <- reactive({
+        apportion(states = state_data(),
+                     nseats = min(max(input[["seats"]], min_seats, na.rm = TRUE), max_seats),
+                     new_states = input[["new_states"]])
+    })
+    output[["add_state"]] <- renderUI({
+        # ch <- setdiff(state_data()[["State"]], state_list)
+        ch <- setdiff(states[["State"]], state_list)
+        checkboxGroupInput("new_states", label = "New States", choices = ch)
+    })
+    output[["apportionment"]] <- DT::renderDataTable({
+        DT::datatable(app(), options = list(pageLength = nrow(app()))) %>%
+            formatPercentage(c("Population %", "Seat %", "Electoral Vote %", "Rep Factor", "EV Factor"), digits = 2) %>%
+            formatRound("Population", mark = ",", digits = 0)
+    })
+    output[["election_results"]] <- renderPrint({
+        prw <- input[["pr_win"]]
+        get_ev_results(election_results(), app(), prw)
+    })
+    output[["award_pr"]] <- renderUI({
+        if ("Puerto Rico" %in% input[["new_states"]]) {
+            choices <- sort(unique(election_results()[["Winner"]]))
+            radioButtons("pr_win", "Award Puerto Rico to:", choices, inline = TRUE)
+        }
+    })
+    output[["chooser"]] <- renderUI({
+        er <- election_results()
+        if ("Puerto Rico" %in% input[["new_states"]]) {
+            new_dat <- list(Year = 0, State = "Puerto Rico", Winner = min(er[["Winner"]]))
+            er <- rbind(er, as.data.frame(new_dat))
+        }
+        st <- with(er, split(State, Winner))
+        candidate <- names(st)
+        chooserInput("assign_state", candidate[[1]], candidate[[2]],
+                     st[[1]],
+                     st[[2]],
+                     size = 20,
+                     multiple = TRUE)
+    })
+
+    output[["custom_election"]] <- renderPrint({
+        er <- election_results()
+        st <- with(er, split(State, Winner))
+        st[[1]] <- c(st[[1]], "Puerto Rico")
+        candidate <- names(st)
+
+        l <- input[["assign_state"]][["left"]]
+        r <- input[["assign_state"]][["right"]]
+        er2 <- data.frame(State = c(l, r),
+                         Winner = ifelse(c(l, r) %in% l,
+                                         candidate[[1]],
+                                         candidate[[2]]))
+        get_ev_results(er2, app(), NULL)
+        })
+
+}
+
+shinyApp(ui = ui, server = server)
---
+++ apportion.R
@@ -0,0 +1,67 @@
+state_list <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California",
+                "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+                "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+                "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+                "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+                "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+                "New Mexico", "New York", "North Carolina", "North Dakota",
+                "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+                "Vermont", "Virginia", "Washington", "West Virginia",
+                "Wisconsin", "Wyoming")
+
+apportion <- function(states,
+                      nseats = 435,
+                      new_states = NULL) {
+
+  seat_cap <- max(nseats * (ceiling(states[["Pop"]] / sum(states[["Pop"]], na.rm = TRUE) * 100) + 5), na.rm = TRUE) %/% 100
+  n <- seq_len(seat_cap)
+  mult <- 1 / sqrt(n * (n - 1))
+  priority <- outer(states[["Pop"]], mult)
+  rownames(priority) <- states[["State"]]
+  st_dat <- states[["State"]]
+  non_states <- setdiff(st_dat, c(state_list, new_states))
+  priority[non_states, ] <- 0
+  # if ((!"District of Columbia" %in% new_states) && "District of Columbia" %in% rownames(priority)) {
+  #   priority["District of Columbia", seq_len(ncol(priority))] <- 0
+  # }
+  # if ((!"Puerto Rico" %in% new_states) && "Puerto Rico" %in% rownames(priority)) {
+  #   priority["Puerto Rico", seq_len(ncol(priority))] <- 0
+  # }
+  ap <- order(priority, decreasing = TRUE)[seq_len(nseats)]
+  seats <- table(rep(states[["State"]], length.out = max(ap, na.rm = TRUE))[ap],
+                 dnn = "State")
+  df <- as.data.frame(seats, responseName = "Seats")
+  df[["Population"]] <- states[["Pop"]]
+  df[c("Population %", "Seat %", "Rep Factor")] <- list(
+    pop_prop <- states[["Pop"]] / sum(states[["Pop"]], na.rm = TRUE),
+    seat_prop <- seats / nseats,
+    seat_prop / pop_prop)
+  ev <- df[["Seats"]] + 2 * (df[["Seats"]] > 0)
+  if ("District of Columbia" %in% non_states) {
+    ev[df[["State"]] == "District of Columbia"] <- 3
+  }
+  df[c("Electoral Votes", "Electoral Vote %")] <- list(ev,
+                                                       ev_prop <- ev / sum(ev, na.rm = TRUE))
+  df[["EV Factor"]] <- ev_prop / pop_prop
+  df <- df[, c("State", "Population", "Population %", "Seats", "Seat %", "Rep Factor", "Electoral Votes", "Electoral Vote %", "EV Factor")]
+  rbind(df,
+        data.frame("State" = "United States",
+                   "Population" = sum(states[["Pop"]]),
+                   "Population %" = 1,
+                   "Seats" = nseats,
+                   "Seat %" = 1,
+                   "Rep Factor" = 1,
+                   "Electoral Votes" = sum(ev, na.rm = TRUE),
+                   "Electoral Vote %" = 1,
+                   "EV Factor" = 1, check.names = FALSE))
+}
+
+get_ev_results <- function(election_results, apportionment, prw) {
+  if (!is.null(prw)) {
+    new_dat <- list(Year = 0, State = "Puerto Rico", Winner = prw)
+    election_results <- rbind(election_results, as.data.frame(new_dat))
+  }
+  full <- merge(election_results, apportionment)
+  with(full, tapply(`Electoral Votes`, Winner, sum))
+}
